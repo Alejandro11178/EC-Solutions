ab62369c3e18084f3a519170e2d327449476b132
Alejandro11178 <aleaafaros@gmail.com>
2023-11-03 01:04:55 -0300

    Creación de profesores

--- proyecto/models.py
+++ proyecto/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+class usuario(models.Model):
+    nombre= models.CharField(max_length=50)
+    apellido= models.CharField(max_length=50)
+    correo= models.EmailField(max_length=50)
+    contraseña = models.CharField(max_length=50)
+    es_profesor = models.BooleanField()
+    materia = models.CharField(max_length=255)
+    nivel = models.CharField(max_length=255)
+    horario = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=6, decimal_places=0)
--- proyecto/views.py
+++ proyecto/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate,login
 from django.contrib import messages
-
+from .models import usuario
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
@@ -11,15 +11,15 @@ def registro(request):
     data = {
         'form': CustomUserCreationForm()
     }
-    
+
     if request.method == 'POST':
         formulario = CustomUserCreationForm(data=request.POST)
         if formulario.is_valid():
-            formulario.save()
-            user = authenticate(username = formulario.cleaned_data["username"], password = formulario.cleaned_data["password1"])
-            login(request)
-            
-            return redirect(to="home")
+            usuario = formulario.save()
+            user = authenticate(username=usuario.username, password=formulario.cleaned_data["password1"])
+            if user is not None:
+                login(request, user)  # Autenticar al usuario
+                return redirect('home')
         else:
             data['form'] = formulario
 
@@ -27,5 +27,24 @@ def registro(request):
 
 
 
-def login(request):
+
+
+def user_login(request):
     return render(request, 'registration/login.html')
+
+def registrar(request):
+    registro = request.POST
+    
+    registro_nuevo = usuario.objects.create(
+        nombre=registro['nombre'],
+        apellido=registro['apellido'],
+        correo=registro['correo'],
+        contraseña=registro['contraseña'],
+        es_profesor = registro['es_profesor'],
+        materia = registro['materia'],
+        nivel = registro['nivel'],
+        horario = registro['horario'],
+        precio=registro['registro']
+    )
+    return render(request, 'registration/registro.html')
+    
