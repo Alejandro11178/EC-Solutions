acc99866de7ec04499a70cfb0ed54210083ff614
Alejandro11178 <aleaafaros@gmail.com>
2023-11-15 16:55:09 -0300

    Segundo avance

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import usuario
-# Register your models here.
 
-# Registra el modelo 'usuario' en la interfaz de administración de Django.
-admin.site.register(usuario)
+# Register your models here.
