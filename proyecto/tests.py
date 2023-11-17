b1e002d25625de6d2403456935b624d513dad627
DiegoHidalgoUSM <127759412+DiegoHidalgoUSM@users.noreply.github.com>
2023-11-17 17:02:15 -0300

    Update tests.py

--- proyecto/tests.py
+++ proyecto/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from usuario.models import usuario
+from room.models import Room, Message
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class HomeViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+
+        self.user = usuario.objects.create(username='testuser', password='testpassword')
+        self.client.force_login(self.user)
+
+    def test_home_view_returns_200(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+class RegistroViewTest(TestCase):
+    def test_registro_view_returns_200(self):
+        response = self.client.get(reverse('registro'))
+        self.assertEqual(response.status_code, 200)
+
+class ChatPaiViewTest(TestCase):
+    def setUp(self):
+        self.user = usuario.objects.create(username='testuser', password='testpassword')
+        self.client.force_login(self.user)
+
+    def test_chat_pai_view_returns_200(self):
+        response = self.client.get(reverse('chat_pai'))
+        self.assertEqual(response.status_code, 200)
+
