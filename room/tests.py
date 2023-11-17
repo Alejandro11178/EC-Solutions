2f32eb9bac6f5b85269293c373a53950f100e43d
DiegoHidalgoUSM <127759412+DiegoHidalgoUSM@users.noreply.github.com>
2023-11-17 18:01:34 -0300

    Update tests.py

--- room/tests.py
+++ room/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Room, Message
 
-# Create your tests here.
+class RoomModelTest(TestCase):
+    def test_room_creation(self):
+        room = Room.objects.create(name='Sala de Pruebas', slug='sala-de-pruebas')
+        self.assertEqual(room.name, 'Sala de Pruebas')
+        self.assertEqual(room.slug, 'sala-de-pruebas')
+
+    def test_room_str_method(self):
+        room = Room.objects.create(name='Otra Sala', slug='otra-sala')
+        self.assertEqual(str(room), 'Otra Sala')
+class MessageModelTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email='testuser@example.com',
+            username='testuser',
+            nombre='Test',
+            apellidos='User',
+            password='testpassword'
+        )
+        self.room = Room.objects.create(name='Sala de Pruebas', slug='sala-de-pruebas')
